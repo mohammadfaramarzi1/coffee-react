0e2c7fe4c9bc6fbfb435eefa3abf025027f303e8
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-01-11 11:35:03 +0330

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brown: {
+          dark: "#4F2C19",
+          medium: "#B0A27B",
+          light: "#E2DFD5",
+        },
+      },
+      fontFamily: {
+        lale: "lale",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+  },
+  plugins: [],
+};
