e35c32a76f3e467e25a55a3a3b13bc2d668aa0ab
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-01-13 08:53:51 +0330

    feat: responsive mobile nav and mobile basket

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,37 @@ import { Link, NavLink } from "react-router-dom";
 function Header() {
   const overlayElem = useRef(null);
   const cardBasketElem = useRef(null);
+  const cardBasketMobileElem = useRef(null);
+  const mobileNavElem = useRef(null);
 
   const overlayHandler = () => {
     overlayElem.current.classList.add("overlay--show");
     cardBasketElem.current.classList.add("card-basket--show");
   };
 
+  const mobileOverlayHandler = () => {
+    overlayElem.current.classList.add("overlay--show");
+    cardBasketMobileElem.current.classList.add("card-basket-mobile--show");
+  };
+
   const closeBasketHandler = () => {
     overlayElem.current.classList.remove("overlay--show");
     cardBasketElem.current.classList.remove("card-basket--show");
+  };
+
+  const closeBasketMobileHandler = () => {
+    overlayElem.current.classList.remove("overlay--show");
+    cardBasketMobileElem.current.classList.remove("card-basket-mobile--show");
+  };
+
+  const closeMobileNavHandler = () => {
+    overlayElem.current.classList.remove("overlay--show");
+    mobileNavElem.current.classList.remove("mobile-nav--show");
+  };
+
+  const showMobileNavHandler = () => {
+    overlayElem.current.classList.add("overlay--show");
+    mobileNavElem.current.classList.add("mobile-nav--show");
   };
 
   return (
@@ -114,19 +136,90 @@ function Header() {
             Mobile Header      
       */}
       <div className="flex md:hidden justify-between items-center bg-brown-medium px-4">
-        <div className="bg-brown-light rounded-full p-1 cursor-pointer flex items-center justify-center">
+        <div
+          onClick={showMobileNavHandler}
+          className="bg-brown-light rounded-full p-1 cursor-pointer flex items-center justify-center transition-all delay-75"
+        >
           <svg className="w-6 h-6">
             <use href="#3-line-menu"></use>
           </svg>
+        </div>
+        <div ref={mobileNavElem} className="mobile-nav">
+          <button
+            onClick={closeMobileNavHandler}
+            className="bg-white text-brown-medium rounded-full px-2 text-xl hover:text-brown-dark transition-colors"
+          >
+            x
+          </button>
+          <ul className="flex flex-col gap-y-5 mt-8 child:text-lg">
+            <li>
+              <NavLink
+                to="/"
+                className="hover:text-[#ebdcad] transition-colors"
+              >
+                صفحه اصلی
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/products"
+                className="hover:text-[#ebdcad] transition-colors"
+              >
+                لیست محصولات
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/services"
+                className="hover:text-[#ebdcad] transition-colors"
+              >
+                خدمات
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                className="hover:text-[#ebdcad] transition-colors"
+              >
+                درباره ما
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/contact"
+                className="hover:text-[#ebdcad] transition-colors"
+              >
+                تماس با ما
+              </NavLink>
+            </li>
+          </ul>
         </div>
         <Link to="/" className="flex items-center">
           <img src="/openmoji_roasted-coffee-bean.png" alt="" />
           <h1 className="text-xl">میم کافه</h1>
         </Link>
-        <div className="bg-brown-light rounded-full p-1 cursor-pointer flex items-center justify-center">
+        <div
+          onClick={mobileOverlayHandler}
+          className="bg-brown-light rounded-full p-1 cursor-pointer flex items-center justify-center"
+        >
           <svg className="w-6 h-6">
             <use href="#basket"></use>
           </svg>
+        </div>
+        <div className="card-basket-mobile" ref={cardBasketMobileElem}>
+          <button
+            onClick={closeBasketMobileHandler}
+            className="text-xl bg-white rounded-full px-2 text-brown-medium hover:text-brown-dark transition-colors"
+          >
+            x
+          </button>
+          <p className="mt-4">هیچ محصولی در سبد خرید وجود ندارد.</p>
+          <Link
+            to="/basket"
+            className="block w-full text-center bg-white rounded-lg py-2 text-brown-medium hover:text-brown-dark transition-colors"
+          >
+            سبد خرید
+          </Link>
         </div>
       </div>
       <div className="overlay" ref={overlayElem}></div>
