9b19c53b08a5b7c4f18c29f538456c4bcc65e7b4
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-01-18 13:03:02 +0330

    feat: ProdutcsList template

---
+++ src/components/ProductsList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ProductBox from "./ProductBox";
+
+function ProductsList() {
+  return (
+    <div className="flex flex-wrap gap-y-5 justify-center sm:justify-between">
+      <ProductBox />
+      <ProductBox />
+      <ProductBox />
+      <ProductBox />
+      <ProductBox />
+      <ProductBox />
+      <ProductBox />
+      <ProductBox />
+      <ProductBox />
+      <ProductBox />
+      <ProductBox />
+      <ProductBox />
+      <ProductBox />
+      <ProductBox />
+    </div>
+  );
+}
+
+export default ProductsList;
--- src/pages/ProductsListPage.jsx
+++ src/pages/ProductsListPage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import ProductBox from "../components/ProductBox";
+import ProductsList from "../components/ProductsList";
 
 function ProductsListPage() {
   return (
-    <div>ProductsListPage</div>
-  )
+    <div className="container my-10">
+      <ProductsList />
+      <div>
+        <div>
+          <h5>همه ی محصولات</h5>
+          
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ProductsListPage
+export default ProductsListPage;
