81abdafbf877ea49fbc39efe09e1832e776f7813
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-01-18 13:20:58 +0330

    feat: CategoryList template"

---
+++ src/components/CategoryList.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function CategoryList() {
+  return (
+    <div className="bg-brown-medium mb-10 py-4 px-2 rounded-md flex justify-between">
+      <div className="flex gap-x-1">
+        <svg className="w-6 h-6">
+          <use href="#category"></use>
+        </svg>
+        <h5>دسته بندی محصولات</h5>
+      </div>
+      <ul className="flex items-center gap-x-4">
+        <li>
+          <button className="text-white">همه ی محصولات</button>
+        </li>
+        <li>
+          <button className="text-white">دانه قهوه</button>
+        </li>
+        <li>
+          <button className="text-white">پودریجات</button>
+        </li>
+        <li>
+          <button className="text-white">اکسسوری</button>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default CategoryList;
--- src/pages/ProductsListPage.jsx
+++ src/pages/ProductsListPage.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import ProductBox from "../components/ProductBox";
 import ProductsList from "../components/ProductsList";
+import { Link } from "react-router-dom";
+import CategoryList from "../components/CategoryList";
 
 function ProductsListPage() {
   return (
     <div className="container my-10">
+      <CategoryList />
       <ProductsList />
-      <div>
-        <div>
-          <h5>همه ی محصولات</h5>
-          
-        </div>
-      </div>
     </div>
   );
 }
