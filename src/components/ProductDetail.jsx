3cc5dec791e0c103032cad46cc3e97b8f835d31e
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-01-19 11:19:00 +0330

    feat: ProductDetailDesc template

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ProductDetailFeatures from "./ProductDetailFeatures";
+import ProductDetailDesc from "./ProductDetailDesc";
 
 function ProductDetail() {
   return (
     <div>
       <ProductDetailFeatures />
+      <ProductDetailDesc />
     </div>
   );
 }
