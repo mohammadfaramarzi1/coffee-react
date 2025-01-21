bff0548477c8aee8b0ba6ddd95e7f84af334bbd4
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-01-21 11:55:49 +0330

    refactor: dynamic ProductDetailSuggest

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -4,6 +4,7 @@ import ProductDetailDesc from "./ProductDetailDesc";
 import ProductDetailSuggest from "./ProductDetailSuggest";
 import { useProducts } from "../context/ProductsProvider";
 import { useParams } from "react-router-dom";
+import Loader from "./Loader";
 
 function ProductDetail() {
   const [state, dispatch, products] = useProducts();
@@ -12,11 +13,19 @@ function ProductDetail() {
   const mainProduct = products.find((product) => product.id === id);
 
   return (
-    <div>
-      <ProductDetailFeatures product={mainProduct} dispatch={dispatch} />
-      <ProductDetailDesc productDesc={mainProduct.desc} />
-      <ProductDetailSuggest products={products} dispatch={dispatch} />
-    </div>
+    <>
+      {mainProduct ? (
+        <div>
+          <ProductDetailFeatures product={mainProduct} dispatch={dispatch} />
+          <ProductDetailDesc productDesc={mainProduct.desc} />
+          <ProductDetailSuggest products={products} dispatch={dispatch} />
+        </div>
+      ) : (
+        <div className="w-full text-center mt-16">
+          <Loader />
+        </div>
+      )}
+    </>
   );
 }
 
--- src/components/ProductDetailSuggest.jsx
+++ src/components/ProductDetailSuggest.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import Heading from "../shared/Heading";
 import ProductBox from "../components/ProductBox";
 
-function ProductDetailSuggest() {
+function ProductDetailSuggest({ products }) {
   return (
     <div className="container">
       <Heading title="مصحولات مرتبط" />
       <div className="flex justify-center sm:justify-between gap-3 flex-wrap my-5">
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
+        {products.slice(0, 4).map((product) => (
+          <ProductBox key={product.id} {...product} />
+        ))}
       </div>
     </div>
   );
