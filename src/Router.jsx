b9e6ff37068d6569dd8841a9cfb799403b21bda9
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-01-28 12:42:34 +0330

    feat: handle logic login

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,7 +6,6 @@ import AboutUsPage from "./pages/AboutUsPage";
 import ContactUsPage from "./pages/ContactUsPage";
 import ProductsListPage from "./pages/ProductsListPage";
 import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
 import BasketPage from "./pages/BasketPage";
 import ArticlesPage from "./pages/ArticlesPage";
 import ProductDetail from "./components/ProductDetail";
@@ -21,7 +20,6 @@ function Router() {
       <Route path="/products" element={<ProductsListPage />} />
       <Route path="/products/:id" element={<ProductDetail />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
       <Route path="/basket" element={<BasketPage />} />
       <Route path="/articles" element={<ArticlesPage />} />
       <Route path="/articles/:id" element={<ArticlesDetail />} />
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import { addToLocalStorage } from "../utils/localStorage";
 
-function Login() {
+function Register() {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+  const changeHandler = (event) => {
+    const name = event.target.name;
+    console.log(name);
+    const value = event.target.value.trim();
+    setForm((form) => ({ ...form, [name]: value }));
+  };
+  const clickHandler = () => {
+    addToLocalStorage("user", form);
+    toast.success("با موفقیت ثبت نام کردید. خوش آمدید");
+    setTimeout(() => {
+      navigate("/");
+    }, 2000);
+  };
   return (
     <div className="bg-[#FFF8F5] flex flex-col justify-center items-center text-center h-screen">
       <div className="pt-5">
@@ -14,41 +36,47 @@ function Login() {
           <h1 className="text-3xl">میم کافه</h1>
         </Link>
       </div>
-      <div className="mt-16">
+      <form className="mt-16" onSubmit={(event) => event.preventDefault()}>
         <div className="bg-brown-light w-16 h-16 rounded-full mx-auto">
           <svg className="w-[66px] h-[50px]">
             <use href="#category-coffee"></use>
           </svg>
         </div>
-        <h2 className="my-5 text-xl">ورود</h2>
+        <h2 className="my-5 text-xl">ثبت نام / ورود</h2>
         <input
           type="text"
-          placeholder="نام کاربری"
+          placeholder="نام و نام خانوادگی"
+          name="name"
           className="block my-5 border border-brown-dark rounded-md px-4 py-1 outline-none"
+          value={form.name}
+          onChange={(event) => changeHandler(event)}
         />
         <input
           type="text"
+          name="email"
           placeholder="ایمیل"
           className="block my-5 border border-brown-dark rounded-md px-4 py-1 outline-none"
+          value={form.email}
+          onChange={(event) => changeHandler(event)}
         />
         <input
           type="password"
           placeholder="رمز"
+          name="password"
           className="block my-5 border border-brown-dark rounded-md px-4 py-1 outline-none"
+          value={form.password}
+          onChange={(event) => changeHandler(event)}
         />
-        <button className="bg-brown-medium w-full rounded-md text-white hover:text-brown-dark transition-colors">
-          ورود
-        </button>
-        <p className="mt-5 mb-1">آیا حساب کاربری ندارید؟</p>
-        <Link
-          to="/register"
-          className="block bg-brown-medium w-full rounded-md text-white hover:text-brown-dark transition-colors"
+        <button
+          onClick={clickHandler}
+          className="bg-brown-medium w-full rounded-md text-white hover:text-brown-dark transition-colors"
         >
           ثبت نام
-        </Link>
-      </div>
+        </button>
+      </form>
+      <ToastContainer position="top-center" autoClose={2000} />
     </div>
   );
 }
 
-export default Login;
+export default Register;
