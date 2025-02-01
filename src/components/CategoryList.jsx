22b75e24887a0e86f04bf92b821ac676879dce8e
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-02-01 15:03:21 +0330

    feat: handle category list

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,25 +1,74 @@
-import React from "react";
+import React, { useRef } from "react";
 import { BiCategoryAlt } from "react-icons/bi";
 
-function CategoryList() {
+function CategoryList({ setSearchParams }) {
+  const categoryRef = useRef(null);
+  console.log(categoryRef);
+
+  const clickHandler = (event) => {
+    const elem = event.target;
+    const tagName = elem.tagName;
+    console.log(categoryRef.current.childNodes);
+    if (!tagName === "LI") return;
+
+    categoryRef.current.childNodes.forEach((item) => {
+      if (item.classList.contains("active")) {
+        item.classList.remove("active");
+        item.classList.add("text-white");
+        console.log(categoryRef.current.childNodes);
+      }
+    });
+    elem.classList.add("active");
+    elem.classList.remove("text-white");
+    setSearchParams(elem.dataset.cat);
+  };
+
   return (
     <div className="bg-brown-medium py-4 px-2 rounded-md flex flex-col gap-y-2 sm:gap-y-0 sm:flex-row justify-between">
       <div className="flex gap-x-1 items-center">
         <BiCategoryAlt />
         <h5>دسته بندی محصولات</h5>
       </div>
-      <ul className="flex items-center gap-x-4">
-        <li>
-          <button className="text-white hover:text-brown-light transition-colors">همه ی محصولات</button>
+      <ul
+        ref={categoryRef}
+        className="flex items-center justify-center sm:justify-between gap-x-4"
+        onClick={clickHandler}
+      >
+        <li
+          data-cat="all"
+          className="active hover:text-[#ebdcad] transition-colors cursor-pointer"
+        >
+          همه ی محصولات
         </li>
-        <li>
-          <button className="text-white hover:text-brown-light transition-colors">دانه قهوه</button>
+        <li
+          data-cat="melo"
+          className="text-white hover:text-[#ebdcad] transition-colors cursor-pointer"
+        >
+          ملو
         </li>
-        <li>
-          <button className="text-white hover:text-brown-light transition-colors">پودریجات</button>
+        <li
+          data-cat="lavaza"
+          className="text-white hover:text-[#ebdcad] transition-colors cursor-pointer"
+        >
+          لاوازا
         </li>
-        <li>
-          <button className="text-white hover:text-brown-light transition-colors">اکسسوری</button>
+        <li
+          data-cat="bonmano"
+          className="text-white hover:text-[#ebdcad] transition-colors cursor-pointer"
+        >
+          بن مانو
+        </li>
+        <li
+          data-cat="mesta"
+          className="text-white hover:text-[#ebdcad] transition-colors cursor-pointer"
+        >
+          مستا
+        </li>
+        <li
+          data-cat="peru"
+          className="text-white hover:text-[#ebdcad] transition-colors cursor-pointer"
+        >
+          پرو
         </li>
       </ul>
     </div>
--- src/pages/ProductsListPage.jsx
+++ src/pages/ProductsListPage.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductsList from "../components/ProductsList";
 import CategoryList from "../components/CategoryList";
 import Search from "../components/Search";
 import { useProducts } from "../context/ProductsProvider";
 import Loader from "../components/Loader";
+import { useSearchParams } from "react-router-dom";
 
 function ProductsListPage() {
   const [state, dispatch, products] = useProducts();
+  const [dispalyed, setDisplayed] = useState([]);
+
+  const [searchParams, setSearchParams] = useSearchParams({});
+  const query = {};
+
+  useEffect(() => {
+    setDisplayed(products);
+  }, []);
 
   return (
     <div className="container my-10">
-      <CategoryList />
+      <CategoryList setSearchParams={setSearchParams} />
       <Search />
       {products ? <ProductsList products={products} /> : <Loader />}
     </div>
